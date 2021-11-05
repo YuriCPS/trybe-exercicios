c6b8616dafda816ea2c85700a5dc8b695a038a80
Yuri Carvalho <yuricps@gmail.com>
2021-11-05 19:06:31 -0300

    Programa 1: OK

--- fundamentos/bloco-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/1-4.js
+++ fundamentos/bloco-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/1-4.js
@@ -2,8 +2,8 @@
 //Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados.
 //Faça programas para: Adição (a + b), Subtração (a - b), Multiplicação (a * b), Divisão (a / b), Módulo (a % b).
 
-//Adição (a + b)
+//Divisão (a / b)
 const a = 10;
 const b = 20;
 
-console.log(a + b);
+console.log(a / b);
