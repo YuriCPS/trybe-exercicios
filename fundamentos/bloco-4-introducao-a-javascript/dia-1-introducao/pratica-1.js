bd43498c2a7db6b86119cc81cf87cbec11f3d69e
Yuri Carvalho <yuricps@gmail.com>
2021-11-05 15:42:47 -0300

    Inicio do bloco 4

---
+++ fundamentos/bloco-4-introducao-a-javascript/dia-1-introducao/pratica-1.js
@@ -0,0 +1,9 @@
+const myName = "Yuri Carvalho";
+const birthCity = "João Pessoa";
+var birthYear = "1992";
+
+var birthYear = "2030";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
